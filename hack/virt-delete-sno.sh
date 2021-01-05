4e40e3c369c387aa882d96a1b5460e7884f3e752
Alexander Chuzhoy <achuzhoy@redhat.com>
2021-01-04 21:32:24 -0500

    clean network and strip hardcoded names.
    The previously created network will be deleted
    upon running 'make clean'. Also removed hardcoded
    network and vm names.
    
    Signed-off-by: Alexander Chuzhoy <achuzhoy@redhat.com>

--- hack/virt-delete-sno.sh
+++ hack/virt-delete-sno.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-sudo virsh destroy sno-test
-sudo virsh undefine sno-test
+VM_NAME=`awk -F'"' '/^VM_NAME/ {print $(NF-1)}' hack/virt-install-sno-iso-ign.sh`
+sudo virsh destroy $VM_NAME
+sudo virsh undefine $VM_NAME
+
+NET_NAME=`xmllint  --xpath 'string(//network/name)' hack/net.xml`
+sudo virsh net-destroy $NET_NAME
+sudo virsh net-undefine $NET_NAME
+
 sudo virsh vol-delete --pool default sno-test.qcow2
