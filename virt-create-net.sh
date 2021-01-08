5b11fce4e544789dc0a74cc024650c8080c9ad01
Mark McLoughlin <markmc@redhat.com>
2021-01-08 11:19:36 +0000

    Add some explanatory notes to hack/virt-create-net.sh
    I spent some time debugging DNS weirdness caused by entries in my
    /etc/hosts. It's probably common for people to hit these sort of
    issues, so let's document what the network setup is.

--- virt-create-net.sh
+++ virt-create-net.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+# Create a libvirt virtual network called 'test-net' configured
+# to assign master1.test-cluster.redhat.com/192.168.126.10 to
+# DHCP requests from 52:54:00:ee:42:e1
+# libvirt will also configure dnsmasq (listening on 192.168.126.1)
+# to respond to DNS queries for several hosts under
+# test-cluster.redhat.com with the 192.168.126.10 address.
+# This dnsmasq is also configured to not forward unresolved requests
+# within the test-cluster.redhat.com domain to upstream DNS servers.
+# Finally, we configure NetworkManager to send any DNS queries
+# on this machine for api.test-cluster.redhat.com to the libvirt
+# configured dnsmasq on 192.168.126.1
+
 
 if [ -z ${NET_XML+x} ]; then
 	echo "Please set NET_XML"
