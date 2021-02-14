d38fec126846bdae6c3ac5ac8b00e8ee56238251
Eran Cohen <eranco@redhat.com>
2021-02-14 14:07:09 +0200

    Revert "Updated repo to use the latest bootstrap in place deployment flow"
    This reverts commit 05bb5d9c8d15b84a2016fcc3eb0a67bd9997d225.

--- generate.sh
+++ generate.sh
@@ -20,3 +20,24 @@ fi
 
 OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE="${RELEASE_IMAGE}" \
 ${INSTALLER_BIN} create single-node-ignition-config --dir=${INSTALLER_WORKDIR}
+
+if [ -z ${INSTALLATION_DISK+x} ]; then
+	echo "INSTALLATION_DISK was not set"
+else
+  #Add complete-installation to the bootstrap-in-place-ignition
+	echo "INSTALLATION_DISK set to $INSTALLATION_DISK adding complete-installation.service"
+
+  sed 's,REPLACE_INSTALLATION_DISK,'"$INSTALLATION_DISK"',' ignition-overrides/complete-installation.sh.template > ignition-overrides/complete-installation.sh
+  cp ${INSTALLER_WORKDIR}/bootstrap-in-place-for-live-iso.ign ignition-overrides/
+
+  podman run \
+    --rm \
+    --privileged \
+    --volume "./ignition-overrides:/assets:z" \
+    quay.io/coreos/fcct:release \
+    --pretty \
+    --strict \
+    --files-dir assets \
+    /assets/complete-installation.fcc > ${INSTALLER_WORKDIR}/bootstrap-in-place-for-live-iso.ign
+fi
+
