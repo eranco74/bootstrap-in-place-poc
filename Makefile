d24e884a8ed921ce94a28c434cd4ff2612ec1718
Eran Cohen <eranco@redhat.com>
2021-02-24 21:46:06 +0200

    Updated release image to 0.6.0 and the rhcos image to 4.7

--- Makefile
+++ Makefile
@@ -9,7 +9,7 @@ ifndef PULL_SECRET
 endif
 
 INSTALLATION_DISK ?= /dev/vda
-RELEASE_IMAGE ?= registry.svc.ci.openshift.org/sno-dev/openshift-bip:0.5.0
+RELEASE_IMAGE ?= registry.svc.ci.openshift.org/sno-dev/openshift-bip:0.6.0
 
 ########################
 
--- download_live_iso.sh
+++ download_live_iso.sh
@@ -1,5 +1,5 @@
-export RHCOS_VERSION=${RHCOS_VERSION:-46.82.202009222340-0}
-export BASE_OS_IMAGE=${BASE_OS_IMAGE:-https://releases-art-rhcos.svc.ci.openshift.org/art/storage/releases/rhcos-4.6/${RHCOS_VERSION}/x86_64/rhcos-${RHCOS_VERSION}-live.x86_64.iso}
+export RHCOS_VERSION=${RHCOS_VERSION:-47.83.202102090044-0}
+export BASE_OS_IMAGE=${BASE_OS_IMAGE:-https://releases-art-rhcos.svc.ci.openshift.org/art/storage/releases/rhcos-4.7/${RHCOS_VERSION}/x86_64/rhcos-${RHCOS_VERSION}-live.x86_64.iso}
 
 if [ $# -eq 0 ]; then
     echo "USAGE: $0 download_path"
