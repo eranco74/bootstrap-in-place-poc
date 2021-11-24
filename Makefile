0038cc1f4b1bc1edc4d7485dff9a2bc0328a1a0f
Omer Tuchfeld <otuchfel@redhat.com>
2021-11-24 20:46:38 +0100

    Update version

--- Makefile
+++ Makefile
@@ -15,7 +15,7 @@ ifndef PULL_SECRET
 endif
 
 INSTALLATION_DISK ?= /dev/vda
-RELEASE_IMAGE ?= quay.io/openshift-release-dev/ocp-release:4.9.0-rc.0-x86_64
+RELEASE_IMAGE ?= quay.io/openshift-release-dev/ocp-release:4.9.9-x86_64
 
 ########################
 
--- download_live_iso.sh
+++ download_live_iso.sh
@@ -1,4 +1,4 @@
-export BASE_OS_IMAGE=${BASE_OS_IMAGE:-https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/pre-release/4.9.0-rc.0/rhcos-live.x86_64.iso}
+export BASE_OS_IMAGE=${BASE_OS_IMAGE:-https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/4.9/4.9.0/rhcos-4.9.0-x86_64-live.x86_64.iso}
 
 if [ $# -eq 0 ]; then
     echo "USAGE: $0 download_path"
