9231aa24acaf99cc4d0ded23024dc911a584e6d7
root <root@rdu-infra-edge-12.infra-edge.lab.eng.rdu2.redhat.com>
2023-03-09 09:24:09 -0500

    Updated generate.sh to add the dummy interface to the bootstrap ignition

--- generate.sh
+++ generate.sh
@@ -18,4 +18,18 @@ if [ -z ${INSTALLER_WORKDIR+x} ]; then
 fi
 
 OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE="${RELEASE_IMAGE}" \
+
+if [ ${USE_INTERNAL_IP+x} ]; then
+OPENSHIFT_INSTALL_BOOTSTRAP_NODE_IP=192.168.99.1
+fi
+
 ${INSTALLER_BIN} create single-node-ignition-config --dir="${INSTALLER_WORKDIR}"
+
+if [ ${USE_INTERNAL_IP+x} ]; then
+	echo "adding dummy interface service to ignition file"
+	jq '.systemd.units += [{
+"contents": "[Unit]\nDescription=Create dummy network\nAfter=NetworkManager.service\n\n[Service]\nType=oneshot\nRemainAfterExit=yes\nExecStart=/bin/nmcli conn add type dummy ifname eth10 autoconnect yes save yes con-name internalEtcd ip4 192.168.99.1/30\n\n[Install]\nWantedBy=multi-user.target\n",
+"enabled": true,
+"name": "dummy-network.service"
+}]' "${INSTALLER_WORKDIR}"/bootstrap-in-place-for-live-iso.ign | sponge "${INSTALLER_WORKDIR}"/bootstrap-in-place-for-live-iso.ign
+fi
